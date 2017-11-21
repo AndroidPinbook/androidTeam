7c85f058bf7de8ac1ba8bf67422c79842d229c6a
ugurgog <32577192+ugurgog@users.noreply.github.com>
2017-11-21 21:18:58 +0300

    Ugur first commit

---
+++ app/src/main/java/uur/com/pinbook/EnterPageActivity.java
@@ -0,0 +1,13 @@
+package uur.com.pinbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_page);
+    }
+}
