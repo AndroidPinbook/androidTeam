0c82bc69b289df5801ffc44fbaf6f2a84aa4434e
ASUS <topaloglun@itu.edu.tr>
2018-01-04 01:04:52 +0300

    Menu bar added.

---
+++ app/src/main/java/uur/com/pinbook/fragments/ProfileFragment.java
@@ -0,0 +1,40 @@
+package uur.com.pinbook.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import uur.com.pinbook.Activities.ProfilePageActivity;
+import uur.com.pinbook.R;
+
+import butterknife.ButterKnife;
+
+public class ProfileFragment extends BaseFragment{
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ButterKnife.bind(this, view);
+
+        ( (ProfilePageActivity)getActivity()).updateToolbarTitle("Profile");
+
+
+        return view;
+    }
+
+
+
+}
